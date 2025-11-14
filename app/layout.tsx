717f53d7cfe70c3385780322703671b2390190ca
Cooperativa Electrica <sistemascoopladormida@gmail.com>
2025-11-14 12:21:48 -0300

    analiticas

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type React from "react"
 import type { Metadata } from "next"
 import { Inter } from "next/font/google"
 import "./globals.css"
+import { Analytics } from "@vercel/analytics/next"
 import Chatbot from "@/components/chatbot"
 
 const inter = Inter({ subsets: ["latin"] })
